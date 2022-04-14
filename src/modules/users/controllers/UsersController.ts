20b69dcc1b108bdda90c692ea31a6625003ad4f0
Kaique Pires <kaiqueroberto.pires@gmail.com>
2022-04-14 08:28:02 -0300

    Profile management, routes and profile view and update services

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import CreateUserService from '../services/CreateUserService';
 import ListUserService from '../services/ListUserService';
 
-export default class UsersCrontoller {
+export default class UsersController {
   public async index(request: Request, response: Response): Promise<Response> {
     const listUser = new ListUserService();
     const users = await listUser.execute();
